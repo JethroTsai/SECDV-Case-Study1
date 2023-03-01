666779ecd10087ad263d11532c3404ad20dfe79f
chuakyle <kyle_kenneth_chua@dlsu.edu.ph>
2023-03-01 12:46:42 +0800

    limited users, products, history and logs buttons in respective roles

--- CSSECDV MP/src/View/ClientHome.java
+++ CSSECDV MP/src/View/ClientHome.java
@@ -46,10 +46,10 @@ public class ClientHome extends javax.swing.JPanel {
         Content.add(mgmtLogs, "mgmtLogs");
         
 //        UNCOMMENT TO DISABLE BUTTONS
-//        historyBtn.setVisible(false);
-//        usersBtn.setVisible(false);
+          historyBtn.setVisible(false);
+          usersBtn.setVisible(false);
 //        productsBtn.setVisible(false);
-//        logsBtn.setVisible(false);
+          logsBtn.setVisible(false);
     }
     
     public void showPnl(String panelName){
